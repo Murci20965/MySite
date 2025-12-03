e2fe47bcdb0581600a7cca98ac3d8c1bca0e55fc
Nhlanhla Mokoena <141430799+Murci20965@users.noreply.github.com>
2025-12-03 03:16:46 +0200

    Updated Experience.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,9 +3,9 @@ import { Github, Linkedin, Twitter } from 'lucide-react';
 
 export default function About() {
   return (
-    <section id="about" className="relative pt-32 pb-24 lg:pt-40 lg:pb-32 bg-black overflow-hidden">
+    <section id="about" className="relative pt-32 pb-24 lg:pt-40 lg:pb-32 bg-gradient-to-b from-gray-900/50 to-gray-800/70 overflow-hidden">
       <div className="absolute inset-0 bg-cover bg-center" style={{ backgroundImage: 'url(/aboutPic.png)' }}></div>
-      <div className="absolute inset-0 bg-black/85"></div>
+      <div className="absolute inset-0 bg-gradient-to-b from-gray-900/50 to-gray-800/70"></div>
 
       <div className="relative z-10 max-w-7xl mx-auto px-6 lg:px-8">
         <div className="grid lg:grid-cols-1 gap-12 lg:gap-16 items-center">
